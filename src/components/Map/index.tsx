6e3217649bab53157f6b64af7ea542ecc7446e8e
v-kazantsev <vvs201@mail.ru>
2022-12-29 21:55:12 +0700

    minor fixes

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,15 +4,17 @@ import Routing from './Routing'
 import './style.css'
 
 const Map = () => {
-  
+  const { selectedOrder } = useSelector((state: any) => state.selectedOrder)
+  const { startCoordinates, endCoordinates } = selectedOrder
   return (
     <div className='leaflet-container'>
-    <MapContainer center={[59.84660399, 30.29496392]} zoom={10} scrollWheelZoom={false}>
+    <MapContainer center={[startCoordinates?.lat, startCoordinates?.long]} zoom={11} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Routing />
+      <Marker position={[startCoordinates?.lat, startCoordinates?.long]} />
+      <Marker position={[endCoordinates?.lat, endCoordinates?.long]} />
     </MapContainer>
     </div>
   )
